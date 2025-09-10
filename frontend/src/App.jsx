d17857ee16f8429c1c913fa5d920e35bc22f6abe
MonikaSalunkhe <146409500+MonikaSalunkhe@users.noreply.github.com>
2025-09-10 14:14:13 +0530

    Add files via upload

---
+++ frontend/src/App.jsx
@@ -0,0 +1,17 @@
+import ChatContainer from "./components/ChatContainer"
+// import UserLogin from "./components/UserLogin"
+
+function App() {
+ return (
+  <>
+  {/* <UserLogin/> */}
+  <div className="App">
+    {/* <UserLogin/>  */}
+  <ChatContainer/>
+  </div>
+  
+  </>
+ )
+}
+
+export default App
